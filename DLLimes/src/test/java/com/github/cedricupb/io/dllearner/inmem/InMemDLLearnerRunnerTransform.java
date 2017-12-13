8d94d1b2aa377e1f22eabaccff6e41e3419c137a
mbilal02 <33041282+mbilal02@users.noreply.github.com>
2017-12-13 17:19:03 +0100

    Iconversion is the interface for Class Conversion.

--- DLLimes/src/test/java/com/github/cedricupb/io/dllearner/inmem/InMemDLLearnerRunnerTransform.java
+++ DLLimes/src/test/java/com/github/cedricupb/io/dllearner/inmem/InMemDLLearnerRunnerTransform.java
@@ -4,6 +4,7 @@ import com.github.cedricupb.config.dllearner.DLConfigBuilder;
 import com.github.cedricupb.config.dllearner.IDLConfiguration;
 import com.github.cedricupb.io.dllearner.IDLLearnerJob;
 import com.github.cedricupb.io.dllearner.IDLLearnerRunner;
+import com.github.cedricupb.io.limes.impl.conversion;
 import org.dllearner.kb.OWLFile;
 import org.junit.Test;
 import org.semanticweb.owlapi.model.IRI;
@@ -17,7 +18,7 @@ public class InMemDLLearnerRunnerTransform {
         String uriPrefix = "http://example.com/father#";
 
         OWLFile ks = new OWLFile();
-        ks.setFileName("/Users/cedricrichter/Documents/DL-LIMES/DLLimes/src/test/java/com/github/cedricupb/io/dllearner/inmem/father.owl");
+        ks.setFileName("C:\\Users\\Bilal\\Documents\\GitHub\\PG_DL\\DL-LIMES\\DLLimes\\src\\test\\java\\com\\github\\cedricupb\\io\\dllearner\\inmem\\father.owl");
         ks.init();
 
         IDLConfiguration config = DLConfigBuilder.init()
@@ -37,6 +38,14 @@ public class InMemDLLearnerRunnerTransform {
         OWLClassExpression expr = job.queryResult();
 
         //TODO: Write the transforming here
+        conversion con = new conversion();
+        try {
+            //con.dlExecute();
+            con.owlTOSparql(expr);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
